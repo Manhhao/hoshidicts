0bb056903bd5323a559f7618d6c6b2c2fbc73ac2
Manhhao <manhhaoo.do@gmail.com>
2026-03-27 14:24:59 +0100

    fix swift package

--- Package.swift
+++ Package.swift
@@ -12,30 +12,16 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "Czip",
-            path: "external/zip/src",
-            sources: ["zip.c"],
-            publicHeadersPath: ".",
-            cSettings: [
-                .define("ZIP_SHARED"),
-                .define("ZIP_BUILD_SHARED"),
-                .define("MINIZ_EXPORT", to: "__attribute__((visibility(\"hidden\")))"),
-                .define("MINIZ_DISABLE_ZIP_READER_CRC32_CHECKS"),
-                .define("MINIZ_NO_TIME")
-            ]
-        ),
-        .target(
             name: "CHoshiDicts",
             dependencies: [
                 .product(name: "libzstd", package: "zstd"),
-                "Czip"
             ],
             path: ".",
             sources: ["src"],
             publicHeadersPath: "include",
             cxxSettings: [
                 .headerSearchPath("include"),
-                .headerSearchPath("external/zip/src"),
+                .headerSearchPath("external/libdeflate"),
                 .headerSearchPath("external/utfcpp/source"),
                 .headerSearchPath("external/glaze/include"),
                 .headerSearchPath("external/xxHash"),
