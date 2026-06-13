84316bf51f18069a55c9ff493ea13b48a30afc21
Manhhao <manhhaoo.do@gmail.com>
2026-06-13 02:25:21 +0200

    add c cli

--- Package.swift
+++ Package.swift
@@ -43,7 +43,7 @@ let package = Package(
             name: "hoshidicts",
             dependencies: ["CHoshiDicts"],
             path: ".",
-            sources: ["cli"],
+            sources: ["cli/main.cpp"],
             cxxSettings: [
                 .headerSearchPath("include"),
                 .headerSearchPath("external/utfcpp/source"),
